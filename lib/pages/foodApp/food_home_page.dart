1f6d1c3f8bd7fa45e59d072f179edb1c0834a4ee
MonikaBhasin7 <monikabhasin.sd17@gmail.com>
2022-03-07 23:23:08 +0530

    separation of concerns

--- lib/pages/foodApp/food_home_page.dart
+++ lib/pages/foodApp/food_home_page.dart
@@ -6,28 +6,35 @@ class FoodHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:  [
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: Image.asset('assets/images/hamburg.png'),
-                ),
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: Image.asset('assets/images/shopping-cart.png'),
-                )
-              ],
-            ),
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          children: [
+            FoodToolbar()
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class FoodToolbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children:  [
+        SizedBox(
+          height: 50,
+          width: 50,
+          child: Image.asset('assets/images/hamburg.png'),
+        ),
+        SizedBox(
+          height: 50,
+          width: 50,
+          child: Image.asset('assets/images/shopping-cart.png'),
+        )
+      ],
     );
   }
 
