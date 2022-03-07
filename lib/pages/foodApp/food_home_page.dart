86cdfaae795986215e98fd8b0c5b5185e48fe6cf
MonikaBhasin7 <monikabhasin.sd17@gmail.com>
2022-03-08 00:40:25 +0530

    tabLayout

--- lib/pages/foodApp/food_home_page.dart
+++ lib/pages/foodApp/food_home_page.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutternoob/utils/colors.dart';
 
-class FoodHomePage extends StatelessWidget {
+class FoodHomePage extends StatefulWidget {
+  @override
+  State<FoodHomePage> createState() => _FoodHomePageState();
+}
+
+class _FoodHomePageState extends State<FoodHomePage>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 6, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +29,8 @@ class FoodHomePage extends StatelessWidget {
           children: [
             FoodToolbarWidget(),
             TitleWidget(),
-            SearchWidget()
+            SearchWidget(),
+            TabWidget(),
           ],
         ),
       ),
@@ -27,7 +43,7 @@ class FoodToolbarWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children:  [
+      children: [
         Image.asset('assets/images/hamburg.png'),
         Image.asset('assets/images/shopping-cart.png')
       ],
@@ -40,49 +56,94 @@ class TitleWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.fromLTRB(0, 12, 0, 0),
-      child: Text("Delicious\nfood for you", style: TextStyle(
-          fontSize: 34,
-          fontWeight: FontWeight.bold
-      )),
+      child: Text("Delicious\nfood for you",
+          style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold)),
     );
   }
 }
 
 class SearchWidget extends StatelessWidget {
-@override
-Widget build(BuildContext context) {
-  return Container(
-    margin: EdgeInsets.only(top: 20),
-    decoration: BoxDecoration(
-        color: Color(0xFFEFEEEE),
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
-        )
-    ),
-    child: Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Icon(
-            Icons.search,
-            color: Colors.black.withAlpha(120),
-          ),
-        ),
-        Expanded(
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: "Search",
-              hintStyle: TextStyle(
-                color: Colors.black.withAlpha(120),
-              ),
-              border: InputBorder.none,
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      decoration: BoxDecoration(
+          color: Color(0xFFEFEEEE),
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          )),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Icon(
+              Icons.search,
+              color: Colors.black.withAlpha(120),
             ),
-            onChanged: (String keyword) {},
           ),
-        ),
-      ],
-    ),
-  );
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "Search",
+                hintStyle: TextStyle(
+                  color: Colors.black.withAlpha(120),
+                ),
+                border: InputBorder.none,
+              ),
+              onChanged: (String keyword) {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
 
+class TabWidget extends StatefulWidget {
+  @override
+  State<TabWidget> createState() => _TabWidgetState();
+}
+
+class _TabWidgetState extends State<TabWidget>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 6, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      isScrollable: true,
+      unselectedLabelColor: Colors.black,
+      labelColor: Colors.red,
+      tabs: const [
+        Tab(
+          text: "Foods",
+        ),
+        Tab(
+          text: "Drinks",
+        ),
+        Tab(
+          text: "Snacks",
+        ),
+        Tab(
+          text: "Sauces",
+        ),
+        Tab(
+          text: "Juices",
+        ),
+        Tab(
+          text: "Dessert",
+        ),
+      ],
+      controller: _tabController,
+      indicatorColor: ColorNoob.orange,
+      indicatorSize: TabBarIndicatorSize.tab,
+      indicatorWeight: 3,
+    );
+  }
 }
--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -7,4 +7,6 @@ class ColorNoob {
   static Color light_purple = Colors.deepPurple.shade100;
   static Color medium_purple = Colors.deepPurple.shade400;
   static Color white = Colors.white70;
+  static Color orange = Color(0xFFFA4A0C);
+  static Color grey_text = Color(0xFF9A9A9D);
 }
